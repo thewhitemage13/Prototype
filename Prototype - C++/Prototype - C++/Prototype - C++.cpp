21649838aabb5de389fc22c746d38452f5afb061
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-18 18:09:56 +0200

    Add files via upload

---
+++ Prototype - C++/Prototype - C++/Prototype - C++.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal abstract {
+public:
+    virtual void SetName(string name) {}
+    virtual string GetName() = 0;
+    virtual Animal* clone() const = 0;
+};
+
+class Sheep : public Animal {
+private:
+    string name;
+    Sheep(const Sheep& donor) {
+        this->name = donor.name;
+    }
+public:
+    Sheep() {}
+
+    void SetName(string name) override {
+        this->name = name;
+    }
+
+    string GetName() override {
+        return name;
+    }
+
+    Sheep* clone() const {
+        return new Sheep(*this);
+    }
+};
+
+int main() {
+    Sheep* sheepDonor = new Sheep();
+    sheepDonor->SetName("Dolly");
+
+    Sheep* sheepClone = sheepDonor->clone();
+
+    cout << "sheepDonor: " << sheepDonor->GetName() << "\n";
+    cout << "sheepClone: " << sheepClone->GetName() << "\n";
+}
